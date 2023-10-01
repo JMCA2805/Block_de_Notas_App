45243e82da2d47468282bea94880b31697e99e37
Ochoaadev <136945432+Ochoaadev@users.noreply.github.com>
2023-10-01 09:53:08 -0400

    Update App.jsx
    Importando función de agregar, eliminar, actualizar las notas.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Nav from './components/Nav'
 import Footer from './components/Footer'
+import TaskList from './components/Task'
 
 function App() {
   return (
     <>
       <Nav></Nav>
       <Footer></Footer>
+      <TaskList/>
     </>
   )
 }
