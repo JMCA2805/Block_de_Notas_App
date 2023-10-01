8b2457b12a2a94bf039f114d294595aaa27d41f8
JMCA2805 <josemauricio2805@gmail.com>
2023-10-01 02:04:52 -0400

    Configuración del Tailwind

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,31 @@
 function Footer() {
   return (
     <>
-      <footer>
+      <footer className="bg-slate-950 text-white">
         <span>Block de Notas</span>
         <section>
           <div>
             <a href="https://github.com/JMCA2805">
+              Alfonso Ochoa:
+              <img class="github" alt="Logo de Github" /> -----
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/JMCA2805">
               Jose M. Camacho:
               <img class="github" alt="Logo de Github" /> JMCA2805
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/JMCA2805">
+              Yetzenia Mendoza:
+              <img class="github" alt="Logo de Github" /> -------
+            </a>
+          </div>
+          <div>
+            <a href="https://github.com/JMCA2805">
+              Carlos Pimiento:
+              <img class="github" alt="Logo de Github" /> -------
             </a>
           </div>
         </section>
